ef5fac2ac0988218fd855cf8445981cff72e1e8e
lyndon <lyndon@172.20.10.6>
2018-05-03 11:49:57 -0700

    Tabs

--- Slinger.io/src/Sling.java
+++ Slinger.io/src/Sling.java
@@ -2,92 +2,92 @@ import java.awt.Color;
 
 public class Sling {
 
-private Projectile p; 
-private Color c;
-private int xPos;
-private int health; //0-100
-private boolean isDead;
-
-
-/**
- * 
- * @param p - The type of projectile this sling has
- * @param c - The color type this sling is
- * @param x - The x position of the sling on the game
- */
-public Sling (Projectile p, Color c, int xPos)
-{
-this.p = p;
-this.c = c;
-this.xPos = xPos;
-health = 100;
-isDead = false;
-}
-
-/**
- * 
- * @return - the projectile the sling has
- */
-public Projectile getProjectile()
-{
-return p;
-}
-
-/**
- * 
- * @return - the color type the swing is
- */
-public Color getColor()
-{
-return c;
-}
-
-
-/**
- * 
- * @return - the x position the swing currently is at
- */
-public int xPos()
-{
-return xPos;
-}
-
-
-/**
- * This method returns the 
- * @param amount - number of units to move by
- */
-public void move(int amount)
-{
-xPos+=amount;
-}
-
-
-/**
- * Restored health to max
- */
-public void restoreHealth()
-{
-health = 100;
-}
-
-
-/**
- * 
- */
-public void increaseProjectileStrength()
-{
-p.increasePower(10);
-}
-
-
-public void decreaseHealth(int amount)
-{
-health-=amount;
-if(health<=0)
-{
-isDead = true;
-}
-
-}
+		private Projectile p; 
+		private Color c;
+		private int xPos;
+		private int health; //0-100
+		private boolean isDead;
+		
+		
+		/**
+		 * 
+		 * @param p - The type of projectile this sling has
+		 * @param c - The color type this sling is
+		 * @param x - The x position of the sling on the game
+		 */
+		public Sling (Projectile p, Color c, int xPos)
+		{
+			this.p = p;
+			this.c = c;
+			this.xPos = xPos;
+			health = 100;
+			isDead = false;
+		}
+		
+		/**
+		 * 
+		 * @return - the projectile the sling has
+		 */
+		public Projectile getProjectile()
+		{
+			return p;
+		}
+		
+		/**
+		 * 
+		 * @return - the color type the swing is
+		 */
+		public Color getColor()
+		{
+			return c;
+		}
+		
+		
+		/**
+		 * 
+		 * @return - the x position the swing currently is at
+		 */
+		public int xPos()
+		{
+			return xPos;
+		}
+		
+		
+		/**
+		 * This method returns the 
+		 * @param amount - number of units to move by
+		 */
+		public void move(int amount)
+		{
+			xPos+=amount;
+		}
+		
+		
+		/**
+		 * Restored health to max
+		 */
+		public void restoreHealth()
+		{
+			health = 100;
+		}
+		
+		
+		/**
+		 * 
+		 */
+		public void increaseProjectileStrength()
+		{
+			p.increasePower(10);
+		}
+		
+		
+		public void decreaseHealth(int amount)
+		{
+			health-=amount;
+			if(health<=0)
+			{
+			isDead = true;
+		}
+		
+		}
 }
