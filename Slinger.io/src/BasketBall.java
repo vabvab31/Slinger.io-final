78b0a64c126f5c7e6b5de222cc36b201fd6504b7
matthewlee <matthewlee@10.16.81.234>
2018-05-06 00:10:15 -0700

    Added a few more classes that extend projectile.

---
+++ Slinger.io/src/BasketBall.java
@@ -0,0 +1,4 @@
+
+public class BasketBall extends Projectile{
+
+}
--- Slinger.io/src/Sling.java
+++ Slinger.io/src/Sling.java
@@ -87,7 +87,7 @@ public class Sling {
 		 */
 		public void increaseProjectileStrength()
 		{
-			p.increasePower(10);
+			//p.increasePower(10);
 		}
 		
 		
