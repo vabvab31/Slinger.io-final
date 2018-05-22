10b8bfbb81ad921178dd4816853c78cd0fba87b0
vaibhavseshadri <vaibhavseshadri@10.0.0.173>
2018-05-21 18:49:37 -0700

    hi

--- Slinger.io/src/Desert.java
+++ Slinger.io/src/Desert.java
@@ -9,88 +9,131 @@ import processing.core.PImage;
  *
  */
 public class Desert extends Map{
-	
+
 	private int width = 800;
 	private int height = 900;
 	private PImage photo;
 
-	
+
 	public Desert(Sling player1, Sling player2) {
 		super(player1, player2);
 		setMap();
 	}
-	
+
 	public void setMap() {
-		
-		
-		
-		
-	
-		
+
+
+		Circle cactus1 = new Circle(width/2, 400, 100);
+		cactus1.setFill(37, 122, 43);
+		cactus1.setStroke(37, 122, 43);
+		add(cactus1);
+
+		Circle cactus2 = new Circle(width/2 + 52, 340, 75);
+		cactus2.setFill(37, 122, 43);
+		cactus2.setStroke(37, 122, 43);
+		add(cactus2);
+
+		Circle cactus3 = new Circle(width/2 + 16, 295, 50);
+		cactus3.setFill(37, 122, 43);
+		cactus3.setStroke(37, 122, 43);
+		add(cactus3);
+
 		Circle dune1 = new Circle(width/2 - 200, 580, 175);
 		dune1.setFill(230, 232, 190);
 		dune1.setStroke(230, 232, 190);
 		add(dune1);
-		
+
 		Circle dune2 = new Circle(width/2 + 200, 580, 175);
 		dune2.setFill(230, 232, 190);
 		dune2.setStroke(230, 232, 190);
 		add(dune2);
-		
+
 		Circle dune3 = new Circle(width/2 + 110, 570, 205);
 		dune3.setFill(230, 232, 190);
 		dune3.setStroke(230, 232, 190);
 		add(dune3);
-		
+
 		Circle dune4 = new Circle(width/2 - 110, 570, 205);
 		dune4.setFill(230, 232, 190);
 		dune4.setStroke(230, 232, 190);
 		add(dune4);
-		
+
 		Circle dune5 = new Circle(width/2, 580, 270);
 		dune5.setFill(230, 232, 190);
 		dune5.setStroke(230, 232, 190);
 		add(dune5);
-		
-		
-		
+
+
 		Rectangle ground = new Rectangle(0,550,900,100); //Ground
 		ground.setFill(230, 232, 190);
-		//ground.setStroke(230, 232, 190);
+		ground.setStroke(230, 232, 190);
 		add(ground);
-		
+
 		//Ellipse ground1 = new Ellipse
-		
 
-		
-		
-		
 
-		
-		
-		
-		
-		
-	
 
-	
+		for (int i = 0; i < 10; i ++) {
+
+			int x = (int) (Math.random()* 80 + 360);
+			int y = (int) (Math.random()* 80 + 360);
+
+			Rectangle spike = new Rectangle(x,y,3,3);
+			add(spike);
+
+
+		}
+
+		for (int i = 0; i < 7; i ++) {
+
+			int x = (int) (Math.random()* 55 + 425);
+			int y = (int) (Math.random()* 55 + 305);
+
+			Rectangle spike = new Rectangle(x,y,3,3);
+			add(spike);
+
+
+		}
+
+		for (int i = 0; i < 5; i ++) {
+
+			int x = (int) (Math.random()* 30 + 401);
+			int y = (int) (Math.random()* 30 + 280);
+
+			Rectangle spike = new Rectangle(x,y,3,3);
+			add(spike);
+
+
+		}
+
+
+
+
+
+
+
+
+
+
+
 	}
-	
-//	public void setup(PApplet p) {
-//		photo = p.loadImage("Desert.png");
-//		photo.resize(800, 600);
-//	}
-	
+
+	//	public void setup(PApplet p) {
+	//		photo = p.loadImage("Desert.png");
+	//		photo.resize(800, 600);
+	//	}
+
 	public void draw(PApplet p) {
-		//setup(p);
-		//p.image(photo, 0, 0);
-		
+
+
 		super.draw(p);
-		
-	//	p.arc(a, b, c, d, start, stop);
-		
+
+
+
+
+
 		if (player1.getHealth() > 0) player1.draw(p);
 		if (player2.getHealth() > 0) player2.draw(p);
 	}
-	
+
 }
