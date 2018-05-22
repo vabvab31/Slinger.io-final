f26efc00d2ce482c3625428c8cc3b5cd3d391ecf
matthewlee <matthewlee@172.20.10.10>
2018-05-21 17:20:34 -0700

    Desert Fixed

--- Slinger.io/src/Desert.java
+++ Slinger.io/src/Desert.java
@@ -24,52 +24,30 @@ public class Desert extends Map{
 		
 		
 		
-		Ellipse cactus1 = new Ellipse(width/2, 330, 50, 200);
-		cactus1.setFill(54, 135, 40);
-		cactus1.setStroke(54, 135, 40);
-		add(cactus1);
 		
-		Ellipse cactus2 = new Ellipse(width/2 + 25, 350, 50, 30);
-		cactus2.setFill(54, 135, 40);
-		cactus2.setStroke(54, 135, 40);
-		add(cactus2);
+	
 		
-		Ellipse cactus3 = new Ellipse(width/2 + 47, 320, 20, 70);
-		cactus3.setFill(54, 135, 40);
-		cactus3.setStroke(54, 135, 40);
-		add(cactus3);
-
-		Ellipse cactus4 = new Ellipse(width/2 - 25, 310, 50, 30);
-		cactus4.setFill(54, 135, 40);
-		cactus4.setStroke(54, 135, 40);
-		add(cactus4);
-		
-		Ellipse cactus5 = new Ellipse(width/2 - 47, 280, 20, 70);
-		cactus5.setFill(54, 135, 40);
-		cactus5.setStroke(54, 135, 40);
-		add(cactus5);
-		
-		Ellipse dune1 = new Ellipse(width/2 - 200, 580, 200, 150);
+		Circle dune1 = new Circle(width/2 - 200, 580, 175);
 		dune1.setFill(230, 232, 190);
 		dune1.setStroke(230, 232, 190);
 		add(dune1);
 		
-		Ellipse dune2 = new Ellipse(width/2 + 200, 580, 200, 150);
+		Circle dune2 = new Circle(width/2 + 200, 580, 175);
 		dune2.setFill(230, 232, 190);
 		dune2.setStroke(230, 232, 190);
 		add(dune2);
 		
-		Ellipse dune3 = new Ellipse(width/2 + 110, 570, 190, 220);
+		Circle dune3 = new Circle(width/2 + 110, 570, 205);
 		dune3.setFill(230, 232, 190);
 		dune3.setStroke(230, 232, 190);
 		add(dune3);
 		
-		Ellipse dune4 = new Ellipse(width/2 - 110, 570, 190, 220);
+		Circle dune4 = new Circle(width/2 - 110, 570, 205);
 		dune4.setFill(230, 232, 190);
 		dune4.setStroke(230, 232, 190);
 		add(dune4);
 		
-		Ellipse dune5 = new Ellipse(width/2, 580, 220, 320);
+		Circle dune5 = new Circle(width/2, 580, 270);
 		dune5.setFill(230, 232, 190);
 		dune5.setStroke(230, 232, 190);
 		add(dune5);
@@ -78,7 +56,7 @@ public class Desert extends Map{
 		
 		Rectangle ground = new Rectangle(0,550,900,100); //Ground
 		ground.setFill(230, 232, 190);
-		ground.setStroke(230, 232, 190);
+		//ground.setStroke(230, 232, 190);
 		add(ground);
 		
 		//Ellipse ground1 = new Ellipse
